0b3687c4bc5e2e1a00114b3141aa7f6f6251df9e
IcyDevv <91300031+IcyDevv@users.noreply.github.com>
2021-10-24 15:30:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bloxapi", 
-    version="0.0.1",
-    author="dareal",
-    author_email="sikling247@gmail.com",
+    name="bloxy", 
+    version="1.0.0",
+    author="Icy",
+    author_email="icydevv026@gmail.com",
     description="A modern & simple to use API wrapper that handles ROBLOX interactions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/darealzz/bloxapi",
+    url="https://github.com/IcyDevv/bloxy.py",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
