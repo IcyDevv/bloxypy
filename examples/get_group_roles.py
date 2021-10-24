642a1a801b6c470c16b6fd15b78512f982341a86
IcyDevv <91300031+IcyDevv@users.noreply.github.com>
2021-10-24 13:08:03 -0400

    Update get_group_roles.py

--- examples/get_group_roles.py
+++ examples/get_group_roles.py
@@ -5,15 +5,11 @@ client = bloxypy.client(None)
 
 async def Main():
   group_roles = client.get_group_roles(1) # this gets the roles of the group
-
-
   if group_roles:
     print("group roles found!")
-    return True
+    return True # return true if group i found/roles are found
   else:
     print("no group roles found!")
-    return False 
+    return False # return false if the group doesnt exist
 
-
-
- asyncio.run(Main())   
+ asyncio.run(Main()) # run the function
