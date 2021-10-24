e7e97770125e1abc752f91f6ee1c609e25ebd633
IcyPlayz YT <91300031+IcyDevv@users.noreply.github.com>
2021-10-24 00:36:59 +0000

    Ready for release.

--- bloxy.py/__init__.py
+++ bloxy.py/__init__.py
@@ -1,7 +1,7 @@
 """
-MIT License
+MIT  License
 
-Copyright (c) 2020 darealzz
+Copyright (c2021 1 IcyDvzz
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
